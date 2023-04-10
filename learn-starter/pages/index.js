47a7a8a665f2f709518c6c21ab8cbf05a8d9eef4
Sarah Preston <sarahpreston@Sarahs-MBP.lan>
2023-04-10 16:50:25 -0500

    data fetching

--- learn-starter/pages/index.js
+++ learn-starter/pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,20 @@ export default function Home() {
         <p>
         Just your average nerd, with kaleidoscope interests and an infinity for making things. You can find me tasting my own questionable fermentation projects, watching some cheesy 70's sci-fi movie, doing handicrafts or plant-spotting. Crawling out of a tent, starting a campfire on a cold morning while drinking a cup of coffee is one of my favorite things.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
